dccd6800a17585f01ca6b1fd241ce9e39ea42e95
@hexlet/cli <support@hexlet.io>
2025-03-22 14:01:45 +0300

    submit java-web-ru/middlewares

---
+++ java-web-ru/middlewares/src/main/java/exercise/App.java
@@ -0,0 +1,78 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.PostsController;
+import exercise.controller.RootController;
+import exercise.util.NamedRoutes;
+import io.javalin.rendering.template.JavalinJte;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
+
+        app.get(NamedRoutes.buildPostPath(), PostsController::build);
+        app.post(NamedRoutes.postsPath(), PostsController::create);
+
+        app.get(NamedRoutes.postsPath(), PostsController::index);
+        app.get(NamedRoutes.postPath("{id}"), PostsController::show);
+
+        app.get(NamedRoutes.editPostPath("{id}"), PostsController::edit);
+        app.post(NamedRoutes.postPath("{id}"), PostsController::update);
+
+        // BEGIN
+        app.after(ctx -> {
+            String response = ctx.result();
+            if (response != null || !response.isEmpty()) {
+                String digest = calculateSHA256(response);
+                ctx.header("X-Response-Digest", digest);
+            }
+        });
+        // END
+
+        return app;
+    }
+    private static String calculateSHA256(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Ошибка при вычислении SHA-256 хеша", e);
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
+
+
+
