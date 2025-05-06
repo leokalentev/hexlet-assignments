4e53e0ddcbe5cd51afebdba7eb68573e90fadaea
@hexlet/cli <support@hexlet.io>
2025-05-06 21:49:42 +0300

    submit java-spring-boot-ru/dto-mapper

--- java-spring-boot-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-boot-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -6,27 +6,32 @@ import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductUpdateDTO;
 import exercise.model.Product;
-import org.mapstruct.*;
-
-import static org.mapstruct.ReportingPolicy.IGNORE;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
-        unmappedTargetPolicy = IGNORE
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class ProductMapper {
-
-    @Mapping(source = "name", target = "title")  // Преобразуем поле name в title
+    @Mapping(source = "name",   target = "title")
+    @Mapping(source = "cost",   target = "price")
+    @Mapping(source = "barcode",target = "vendorCode")
     public abstract ProductDTO map(Product product);
 
-    @Mapping(target = "id", ignore = true)  // id не устанавливаем вручную
-    public abstract Product create(ProductCreateDTO productCreateDTO);
+    @Mapping(target = "id",      ignore = true)
+    @Mapping(source = "title",   target = "name")
+    @Mapping(source = "price",   target = "cost")
+    @Mapping(source = "vendorCode", target = "barcode")
+    public abstract Product create(ProductCreateDTO dto);
 
-    public abstract ProductDTO map(ProductCreateDTO productCreateDTO);
-
-    @Mapping(target = "id", ignore = true)  // id игнорируем при обновлении
-    public abstract Product update(ProductUpdateDTO productUpdateDTO, @MappingTarget Product product);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "price", target = "cost")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product product);
 }
-
 // END
