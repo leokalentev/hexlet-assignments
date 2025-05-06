1e7f38ab59430f44d16c2721bccea675d9b837ff
@hexlet/cli <support@hexlet.io>
2025-05-06 21:43:35 +0300

    submit java-spring-boot-ru/dto-mapper

--- java-spring-boot-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-boot-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -6,10 +6,7 @@ import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductUpdateDTO;
 import exercise.model.Product;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 import static org.mapstruct.ReportingPolicy.IGNORE;
 
@@ -19,10 +16,17 @@ import static org.mapstruct.ReportingPolicy.IGNORE;
         unmappedTargetPolicy = IGNORE
 )
 public abstract class ProductMapper {
-    public abstract ProductDTO map(Product product);
-    public abstract Product create(ProductCreateDTO productCreateDTO);
-    public abstract ProductDTO map(ProductCreateDTO productCreateDTO);
-    public abstract Product update(ProductUpdateDTO productUpdateDTO, @MappingTarget Product product);
 
+    @Mapping(source = "name", target = "title")  // Преобразуем поле name в title
+    public abstract ProductDTO map(Product product);
+
+    @Mapping(target = "id", ignore = true)  // id не устанавливаем вручную
+    public abstract Product create(ProductCreateDTO productCreateDTO);
+
+    public abstract ProductDTO map(ProductCreateDTO productCreateDTO);
+
+    @Mapping(target = "id", ignore = true)  // id игнорируем при обновлении
+    public abstract Product update(ProductUpdateDTO productUpdateDTO, @MappingTarget Product product);
 }
+
 // END
